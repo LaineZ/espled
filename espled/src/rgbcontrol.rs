8ac494646d388a4daca59e4ed2947951db56d07d
140bpmdubstep <laineprikol@gmail.com>
2024-12-12 19:08:33 +0300

    idk

--- espled/src/rgbcontrol.rs
+++ espled/src/rgbcontrol.rs
@@ -2,11 +2,6 @@ use crate::rgb::RGBLedColor;
 use esp_idf_hal::ledc::LedcDriver;
 use esp_idf_svc::nvs::{EspNvs, EspNvsPartition, NvsDefault};
 
-const RED_MULTIPLIER: u32 = 255;
-const GREEN_MULTIPLIER: u32 = 255;
-const BLUE_MULTIPLIER: u32 = 255;
-const MULTIPLIER_DIVISOR: u32 = 255;
-
 pub struct RgbControl {
     pwm_r: LedcDriver<'static>,
     pwm_g: LedcDriver<'static>,
@@ -57,13 +52,13 @@ impl RgbControl {
     fn set_color_pwm(&mut self) -> anyhow::Result<()> {
         let max_duty = self.pwm_r.get_max_duty();
         self.pwm_r.set_duty(
-            self.color.red as u32 * RED_MULTIPLIER / MULTIPLIER_DIVISOR * max_duty / 255,
+            self.color.red as u32 * max_duty / 255,
         )?;
         self.pwm_g.set_duty(
-            self.color.green as u32 * GREEN_MULTIPLIER / MULTIPLIER_DIVISOR * max_duty / 255,
+            self.color.green as u32 * max_duty / 255,
         )?;
         self.pwm_b.set_duty(
-            self.color.blue as u32 * BLUE_MULTIPLIER / MULTIPLIER_DIVISOR * max_duty / 255,
+            self.color.blue as u32 * max_duty / 255,
         )?;
         Ok(())
     }
